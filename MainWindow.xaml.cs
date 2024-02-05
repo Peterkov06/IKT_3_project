255b1ffebb9e8eadf5572fa70657bbdb20469864
Peterkov06 <peterkov06@gmail.com>
2024-02-05 10:51:43 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Reflection;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace IKT_3_project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string path = "..\\..\\..\\Launcher\\UIxml.xml";
+            XDocument doc = XDocument.Load(path);
+
+            StackPanel panel = new StackPanel();
+            Grid.SetColumn(panel, 1);
+
+            foreach (XElement element in doc.Root.Elements("Button"))
+            {
+                Button button = new Button { Name = (string)element.Attribute("Name"),
+                    Content = (string)element.Attribute("Content"),
+                    Margin = new Thickness(5),
+                };
+                string methodName = (string)element.Attribute("OnClick");
+                MethodInfo method = GetType().GetMethod(methodName);
+                button.Click += (sender, e) => { method.Invoke(this, new object[] { sender, e }); };
+                panel.Children.Add(button);
+            }
+            MainGrid.Children.Add(panel);
+            doc.Save(path);
+        }
+
+        public void Hello(object sender, RoutedEventArgs eventArgs)
+        {
+            MessageBox.Show("Hello There!");
+        }
+    }
+}
