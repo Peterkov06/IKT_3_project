7bdb053701957e42b13a624a342b0f57c0083997
Kovacs Peter <peterkov06@gmail.com>
2024-02-05 17:07:58 +0100

    Created function to generate basic events, and loads from xml file and database.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Reflection;
+﻿using System.Data.SQLite;
+using System.IO;
+using System.Reflection;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -19,17 +21,17 @@ namespace IKT_3_project
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string dbPath;
         public MainWindow()
         {
             InitializeComponent();
 
             string path = "..\\..\\..\\Launcher\\UIxml.xml";
             XDocument doc = XDocument.Load(path);
+            /*
+            var parentTag = doc.Root.Descendants("MainMenu").FirstOrDefault();
 
-            StackPanel panel = new StackPanel();
-            Grid.SetColumn(panel, 1);
-
-            foreach (XElement element in doc.Root.Elements("Button"))
+            foreach (XElement element in parentTag.Descendants("Button"))
             {
                 Button button = new Button { Name = (string)element.Attribute("Name"),
                     Content = (string)element.Attribute("Content"),
@@ -40,13 +42,76 @@ namespace IKT_3_project
                 button.Click += (sender, e) => { method.Invoke(this, new object[] { sender, e }); };
                 panel.Children.Add(button);
             }
-            MainGrid.Children.Add(panel);
+            MainGrid.Children.Add(panel);*/
+
+            dbPath = doc.Root.Descendants("PathLinks").Descendants("StoryDatabase").Attributes("Path").Select(x=>x.Value).FirstOrDefault();
+
+            if (!File.Exists(dbPath))
+            {
+                SQLiteConnection.CreateFile(dbPath);
+            }
+
+            GeneratePart(1);
+
             doc.Save(path);
         }
 
         public void Hello(object sender, RoutedEventArgs eventArgs)
         {
             MessageBox.Show("Hello There!");
+        }
+
+        public void GeneratePart(int ind)
+        {
+            MainGrid.Children.Clear();
+            string connString = $"Data Source={dbPath};Version=3;";
+            using (SQLiteConnection conn = new SQLiteConnection(connString))
+            {
+                conn.Open();
+                string sqlComm = $"SELECT text FROM Parts Where id = {ind}";
+                using (SQLiteCommand cmd = new(sqlComm, conn))
+                {
+                    using (SQLiteDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            WrapPanel textWrapper = new();
+                            TextBlock test = new() { Text = reader.GetString(0), TextWrapping = TextWrapping.Wrap, Margin = new Thickness(5), FontSize = 16 };
+                            textWrapper.Children.Add(test);
+                            MainGrid.Children.Add(textWrapper);
+                        }
+                    }
+
+                    string sqlComm2 = $"SELECT text, next_part FROM Choiches Where part_id = {ind}";
+                    StackPanel panel = new StackPanel();
+                    Grid.SetColumn(panel, 1);
+
+                    using (SQLiteCommand opts = new SQLiteCommand(sqlComm2, conn))
+                    {
+                        using (SQLiteDataReader r2 = opts.ExecuteReader())
+                        {
+                            while (r2.Read())
+                            {
+                                Button button = new Button
+                                {
+                                    Content = r2.GetString(0),
+                                    FontSize = 12,
+                                    Margin = new Thickness(5),
+                                };
+                                
+                                if (!r2.IsDBNull(1))
+                                {
+                                    int next_id = r2.GetInt32(1);
+                                    button.Click += (sender, e) => { GeneratePart(next_id); };
+                                }
+                                
+                                panel.Children.Add(button);
+                            }
+                            MainGrid.Children.Add(panel);
+                        }
+                    }
+                }
+            }
         }
     }
 }
